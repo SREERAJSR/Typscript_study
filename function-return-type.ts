d82a47c67398ff738224a7c59de15d81074cd10f
SREERAJ <admin@SREERAJ-S-R-MacBook-Air.local>
2025-02-24 23:32:17 +0530

    function return type

---
+++ function-return-type.ts
@@ -0,0 +1,12 @@
+// regular function
+function double1(x:number):number{
+    return x*x
+}
+
+console.log(double1(2));
+
+
+// arrrow function
+
+const double2 = (x:number) :number => x*x;
+console.log(double2(23));
