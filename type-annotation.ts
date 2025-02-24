0e29d49e75061c1f42c940acd1bc6efaf3df981c
SREERAJ <admin@SREERAJ-S-R-MacBook-Air.local>
2025-02-24 21:25:22 +0530

    first commit and type annottation

---
+++ type-annotation.ts
@@ -0,0 +1,16 @@
+// string
+// let name:string ='sreeraj';
+// console.log(name);
+
+// number
+
+// let age:number =21;
+// age +=2;
+// console.log(age);
+
+
+// boolean
+
+// let isMarried :boolean =true;
+// isMarried =false;
+// console.log(isMarried);
