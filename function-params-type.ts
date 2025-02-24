4a27d817c8c905b056c173d202fb5613cc8cea89
SREERAJ <admin@SREERAJ-S-R-MacBook-Air.local>
2025-02-24 21:48:40 +0530

    function-parameter type

---
+++ function-params-type.ts
@@ -0,0 +1,15 @@
+// regular function
+
+function addTwoNumber(num1:number,num2:number){
+    return num1 + num2 ;
+}
+
+let res =addTwoNumber(1,2)
+console.log(res);
+
+// arrow function
+
+const  multiplyFunction =(num1:number,num2:number) => num1 *num2 ;
+
+let res2 = multiplyFunction(10,2)
+console.log(res2);
