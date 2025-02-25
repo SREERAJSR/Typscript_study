3d4634afbabfb04f0f955d0aa3df097e3e2002eb
SREERAJ <admin@SREERAJ-S-R-MacBook-Air.local>
2025-02-25 20:33:53 +0530

    tuples

---
+++ tuples.ts
@@ -0,0 +1,13 @@
+const games :[string, string,string ] = ['game1','game2','game3']
+console.log(games);
+
+const phoneBrands :[string, boolean ,{name:string}] =[
+    'Xiaomi',
+    true,
+    {name:'Oneplus'}
+] 
+
+// if phonebrands = [true,'Xiaomi' ,{name:"oneplus"}] will error  ---> follow the structure 
+
+
+console.log(phoneBrands);
