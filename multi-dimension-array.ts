680c75a800605f22b918ccb7d4cbf4bbc2b58d73
SREERAJ <admin@SREERAJ-S-R-MacBook-Air.local>
2025-02-25 15:32:00 +0530

    multidimension-array

---
+++ multi-dimension-array.ts
@@ -0,0 +1,6 @@
+const singleDi :number[] = [1,23,3,4]
+const multiDi :number[][] =[[2,3,4]] 
+const tripleDi:number[][][] =[[[2,332]]]
+console.log(singleDi);
+console.log(multiDi);
+console.log(tripleDi);
