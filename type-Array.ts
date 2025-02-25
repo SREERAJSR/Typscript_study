18bcffc01e971b14c19037f361c209fecaa4f90c
SREERAJ <admin@SREERAJ-S-R-MacBook-Air.local>
2025-02-25 15:27:21 +0530

    type-Array

---
+++ type-Array.ts
@@ -0,0 +1,10 @@
+// first way
+
+const nums:number[] =[1,2,3,4]
+nums.push(5)
+console.log(nums);
+
+// second way
+const strList:Array<string> =['sreeraj','dev']
+strList.push("tom")
+console.log(strList);
