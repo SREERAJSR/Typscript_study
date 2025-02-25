55f8747dbac3d3efcd61296f73778a1ed184abfc
SREERAJ <admin@SREERAJ-S-R-MacBook-Air.local>
2025-02-25 20:44:18 +0530

    enums

---
+++ enums.ts
@@ -0,0 +1,13 @@
+
+// it is group of named constants
+enum StatusCodes {
+    NOTFOUND =404,
+    OK=200,
+    SUCESS=200,
+    CREATED=202,
+    INTERNAL_SEVER_ERROR=500
+}
+
+const httpRequest:StatusCodes = StatusCodes.NOTFOUND
+
+console.log(`the server responds with an status code:${httpRequest}`);
